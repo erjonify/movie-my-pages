e100c5faf36211286c36041134003d1e2f865800
Erjon Skenderi <erjonify@gmail.com>
2022-01-19 21:16:41 +0100

    installed eslint,prettier, apollo client

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloClient, inMemoryCache } from '@apollo/client'
 
-ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById('root')
-);
+// eslint-disable-next-line react/jsx-filename-extension
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
