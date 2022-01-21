623042ab6a4da03ea330362344a624668a7173ee
Erjon Skenderi <erjonify@gmail.com>
2022-01-21 20:42:17 +0100

    created searchbar component

---
+++ src/components/SearchBox/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const index = () => <div>Hello from search bar</div>
+
+export default index
---
+++ src/containers/App/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SearchBox from '../../components/SearchBox'
+
+function Index() {
+    return <SearchBox />
+}
+
+export default Index
