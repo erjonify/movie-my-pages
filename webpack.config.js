87a5bd2eb82bb6eaeeca20ea76823dd67444e491
Erjon Skenderi <erjonify@gmail.com>
2022-01-22 23:05:43 +0100

    added all images and icons , also did webpack configuration to handle them. installed prop types

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,25 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
         ],
     },
     resolve: {
